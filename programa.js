7794e94a59dfc2c3d0a43f689628efbca2283ad1
Eliana Camila Mora <sigmorab@gmail.com>
2025-02-05 20:49:52 -0500

    v 0.1

---
+++ programa.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([4.737435383610156,-74.09873671319136], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
